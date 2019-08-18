09e49787252d847f076bc6e00943fc02ed894383
Jeff Nesler <jmn86@yahoo.com>
2019-08-18 09:18:18 -0400

    Add project files.

---
+++ AsyncAwaitWinForms/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace AsyncAwaitWinForms
+{
+    partial class FormAsyncAwait
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(94, 40);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(75, 23);
+            this.buttonGo.TabIndex = 0;
+            this.buttonGo.Text = "Go";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.ButtonGo_Click);
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Location = new System.Drawing.Point(110, 106);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(13, 13);
+            this.labelCount.TabIndex = 1;
+            this.labelCount.Text = "0";
+            // 
+            // FormAsyncAwait
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 197);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.buttonGo);
+            this.Name = "FormAsyncAwait";
+            this.Text = "AsyncAwaitDemo";
+            this.Load += new System.EventHandler(this.FormAsyncAwait_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.Label labelCount;
+    }
+}
+
---
+++ AsyncAwaitWinForms/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwaitWinForms
+{
+    public partial class FormAsyncAwait : Form
+    {
+        private readonly SynchronizationContext synchronizationContext;
+        private DateTime previousTime = DateTime.Now;
+        public FormAsyncAwait()
+        {
+            InitializeComponent();
+            synchronizationContext = SynchronizationContext.Current;
+        }
+            
+        private void FormAsyncAwait_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void UpdateUI(int value)
+        {
+            var timeNow = DateTime.Now;
+
+            if ((DateTime.Now - previousTime).Milliseconds <= 50) return;
+
+            synchronizationContext.Post(new SendOrPostCallback(o =>
+            {
+                labelCount.Text = @"Counter " + (int)o;
+            }), value);
+
+            previousTime = timeNow;
+        }
+
+        private async void ButtonGo_Click(object sender, EventArgs e)
+        {
+            buttonGo.Enabled = false;
+            var count = 0;
+
+            await Task.Run(() =>
+            {
+                for (var i = 0; i <= 5000000; i++)
+                {
+                    UpdateUI(i);
+                    count = i;
+                }
+            });
+
+            labelCount.Text = @"Counter " + count;
+            buttonGo.Enabled = true;
+        }
+    }
+}
